b5cafb632a65f87b4b3191e9f458cc9ff1fd89a0
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2026-02-16 20:37:45 -0600

    feat add interactive messages

--- util.py
+++ util.py
@@ -3,6 +3,15 @@ def GetTextUser(message):
     typeMessage = message["type"]
     if typeMessage == "text":
         text = (message["text"])["body"]
+    elif typeMessage == "interactive":
+        interactive = message["interactive"]
+        typeInteractive = interactive["type"]
+        if typeInteractive == "button_reply":
+            text = (interactive["button_reply"])["title"]
+        elif typeInteractive == "list_reply":
+            text = (interactive["list_reply"])["title"]
+        else:
+            print("[!] Interactive type not supported")
     else:
         print("[!] Message type not supported")
     return text
